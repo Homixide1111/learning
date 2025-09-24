775dfa503657ec62015f7c588a0036e10cae879a
only0ne <paulpogba6575@gmail.com>
2025-09-24 21:47:27 +0300

    new branch

--- first.py
+++ first.py
@@ -1,1 +1,15 @@
+a = int(input())
 print('helloworld')
+if a > 0:
+    print('good')
+else:
+    print("bad")
+b = int(input())
+if b > 10:
+    print(b - a)
+else:
+    print(a + b)
+k = []
+for i in range(0, 5):
+    k.append(i)
+print(k)
